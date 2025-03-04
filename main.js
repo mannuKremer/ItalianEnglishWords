dd41ada35bcf162367562795c3bd8a0a0c9631a5
Mannu Kremer <166306972+mannuKremerCb@users.noreply.github.com>
2025-03-04 17:25:21 +0200

    Initial: add application with data from mockup json

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, Tray, Menu } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+const UPDATE_INTERVAL = 30000;
+let words = [];
+
+app.whenReady().then(() => {
+    app.dock.hide();
+    tray = new Tray(path.join(__dirname, 'icon.png'));
+
+    fs.readFile(path.join(__dirname, 'italian_italian_words.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading words file:", err);
+            return;
+        }
+        words = JSON.parse(data);
+        updateWord();
+        setInterval(updateWord, UPDATE_INTERVAL);
+    });
+
+    function updateWord() {
+        if (words.length === 0) return;
+        const randomIndex = Math.floor(Math.random() * words.length);
+        const { italian, english } = words[randomIndex];
+        tray.setTitle(`${italian} - ${english}`);
+    }
+});
